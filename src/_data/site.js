1ed2925b6065b0bddb3a5e0540e7ac3d743a23ea
ed <ed@DESKTOP-53ADDPM>
2023-06-06 16:28:04 -0700

    primer commit

---
+++ src/_data/site.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'Programación Creativa',
+  description: 'Revista mensual ',
+  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  url: 'https://programacioncreativa.netlify.app', // your site url without trailing slash
+  paginate: 6 // how many posts you want to show for each page
+  // uncomment the next line if you want to add disqus to your site
+  // disqusShortname: "your-shortname"
+};
