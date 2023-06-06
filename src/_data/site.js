e0531fcbaae9d2d68383728aeffddb9ab9e8fc03
ed <ed@DESKTOP-53ADDPM>
2023-06-06 16:40:34 -0700

    second commit

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'Programación Creativa',
-  description: 'Revista mensual ',
+  title: 'PC Magazine',
+  description: 'Revista mensual sobre programación y diseño gráfico',
   keywords: ['eleventy', 'template', 'simple', 'clean'],
-  url: 'https://programacioncreativa.netlify.app', // your site url without trailing slash
+  url: 'https://pcmagazine.netlify.app', // your site url without trailing slash
   paginate: 6 // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
   // disqusShortname: "your-shortname"
