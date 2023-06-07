5baa032ed8c17c781a162e192392ff74bfe79be4
ed <ed@DESKTOP-53ADDPM>
2023-06-07 12:13:55 -0700

    5to commit

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'Creativo Digital',
+  title: 'Creativo.Magazine',
   description: 'Revista mensual sobre creatividad, diseño y programación',
   keywords: ['eleventy', 'template', 'simple', 'clean'],
-  url: 'https://mentescreativas.netlify.app', // your site url without trailing slash
+  url: 'https://creativomagazine.netlify.app', // your site url without trailing slash
   paginate: 6 // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
   // disqusShortname: "your-shortname"
