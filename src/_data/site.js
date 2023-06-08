ea2e3f018db652b6838274186edb9c4d9b35e51f
ed <ed@DESKTOP-53ADDPM>
2023-06-07 22:20:51 -0700

    7 commit

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'Ecosistemas Digitales e industria 4.0',
+  title: 'Ecosistemas Digitales e Industria 4.0',
   description: 'Podcast quincenal',
   keywords: ['eleventy', 'template', 'simple', 'clean'],
-  url: 'https://creativomagazine.netlify.app', // your site url without trailing slash
+  url: 'https://creativo40.netlify.app', // your site url without trailing slash
   paginate: 6 // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
   // disqusShortname: "your-shortname"
